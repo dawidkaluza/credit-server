a2b8e3212c583a26287f7f033a3e2d9144bbb190
dawid <daw.kaluza@gmail.com>
2021-04-09 17:29:24 +0200

    Refactor error handling

--- customer-service/src/main/java/pl/dkaluza/customer/controllers/ControllersExceptionsHandler.java
+++ customer-service/src/main/java/pl/dkaluza/customer/controllers/ControllersExceptionsHandler.java
@@ -1,6 +1,7 @@
 package pl.dkaluza.customer.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pl.dkaluza.customer.exceptions.CustomerNotFoundException;
@@ -11,7 +12,10 @@ import java.io.IOException;
 @RestControllerAdvice
 public class ControllersExceptionsHandler {
     @ExceptionHandler(CustomerNotFoundException.class)
-    public void customerNotFoundExcHandler(HttpServletResponse response, CustomerNotFoundException e) throws IOException {
-        response.sendError(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    public ResponseEntity<?> customerNotFoundExcHandler() {
+        return ApiError.builder()
+            .status(HttpStatus.NOT_FOUND)
+            .message("Can't find some customers")
+            .build().toResponseEntity();
     }
 }
